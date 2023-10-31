7bd299be1218fcd7ed8d6e7bd547070a3410961c
Quang22CDTH11 <113655587+quangk22@users.noreply.github.com>
2023-10-31 23:12:59 +0700

    Add files via upload

---
+++ DoThi- Dijkstra/src/quang/dev/Dijkstra.java
@@ -0,0 +1,51 @@
+package quang.dev;
+
+public class Dijkstra {
+	private static final int V = 9;
+
+    private int minDistance(int[] dist, boolean[] sptSet) {
+        int min = Integer.MAX_VALUE, minIndex = -1;
+
+        for (int v = 0; v < V; v++) {
+            if (!sptSet[v] && dist[v] <= min) {
+                min = dist[v];
+                minIndex = v;
+            }
+        }
+
+        return minIndex;
+    }
+
+    private void printSolution(int[] dist) {
+        System.out.println("Vertex \t\t Distance from Source");
+        for (int i = 0; i < V; i++) {
+            System.out.println(i + " \t\t " + dist[i]);
+        }
+    }
+
+    public void dijkstra(int graph[][], int src) {
+        int dist[] = new int[V];
+        boolean sptSet[] = new boolean[V];
+
+        for (int i = 0; i < V; i++) {
+            dist[i] = Integer.MAX_VALUE;
+            sptSet[i] = false;
+        }
+
+        dist[src] = 0;
+
+        for (int count = 0; count < V - 1; count++) {
+            int u = minDistance(dist, sptSet);
+
+            sptSet[u] = true;
+
+            for (int v = 0; v < V; v++) {
+                if (!sptSet[v] && graph[u][v] != 0 && dist[u] != Integer.MAX_VALUE && dist[u] + graph[u][v] < dist[v]) {
+                    dist[v] = dist[u] + graph[u][v];
+                }
+            }
+        }
+
+        printSolution(dist);
+    }
+}
---
+++ DoThi- Dijkstra/src/quang/dev/MainApp.java
@@ -0,0 +1,22 @@
+package quang.dev;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int graph[][] = new int[][] {
+            {0, 4, 0, 0, 0, 0, 0, 8, 0},
+            {4, 0, 8, 0, 0, 0, 0, 11, 0},
+            {0, 8, 0, 7, 0, 4, 0, 0, 2},
+            {0, 0, 7, 0, 9, 14, 0, 0, 0},
+            {0, 0, 0, 9, 0, 10, 0, 0, 0},
+            {0, 0, 4, 14, 10, 0, 2, 0, 0},
+            {0, 0, 0, 0, 0, 2, 0, 1, 6},
+            {8, 11, 0, 0, 0, 0, 1, 0, 7},
+            {0, 0, 2, 0, 0, 0, 6, 7, 0}
+    };
+    Dijkstra t = new Dijkstra();
+    t.dijkstra(graph, 0);
+	}
+
+}
